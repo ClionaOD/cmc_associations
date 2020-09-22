46a8295b79b40ee505557d60cef647c898fd940c
ClionaOD <odoherc1@tcd.ie>
2020-09-22 17:02:15 +0100

    comment for debug

--- get_acts.sh
+++ get_acts.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 #
+
 python3 /home/clionaodoherty/cmc_associations/get_acts.sh \
     --model_path /data/movie-associations/saves/temporal/finetune5min/movie-training-distorted/memory_nce_16384_alexnet_lr_0.03_decay_0.0001_bsz_128_sec_300_view_temporal/ckpt_epoch_80.pth \
     --save_path /home/clionaodoherty/cmc_associations/activations/5min_distort_testCategs.pickle \
-    --image_path /home/clionaodoherty/imagenet_test \
     --transform distort
+    #--image_path /home/clionaodoherty/imagenet_test \
