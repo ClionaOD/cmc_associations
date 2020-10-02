1fbff339d0866690897d41c33763dd41072cae09
ClionaOD <odoherc1@tcd.ie>
2020-10-02 13:04:46 +0100

    supervised on

--- get_acts.sh
+++ get_acts.sh
@@ -6,4 +6,5 @@ python3 /home/clionaodoherty/cmc_associations/get_activations.py \
     --model_path /home/clionaodoherty/cmc_associations/weights \
     --save_path  /home/clionaodoherty/cmc_associations/activations/test \
     --image_path /home/clionaodoherty/cmc_associations/test_imagenet/ \
-    --transform distort 
+    --transform distort \
+    --supervised True
