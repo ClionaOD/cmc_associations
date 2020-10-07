e26f749ab7709a941bd969ad43255ad9c04bb5f6
ClionaOD <odoherc1@tcd.ie>
2020-10-07 19:53:01 +0100

    correct image path with to_test

--- get_rdms.sh
+++ get_rdms.sh
@@ -2,7 +2,7 @@
 
 #WORKSTATION PATHS FOR RDMS
 python3 /home/clionaodoherty/cmc_associations/get_rdms.py \
-    --image_path /data/imagenet_cmc \
+    --image_path /data/imagenet_cmc/to_test \
     --activation_path /home/clionaodoherty/cmc_associations/activations \
     --rdm_path /home/clionaodoherty/cmc_associations/rdms \
     --save_rdm True
