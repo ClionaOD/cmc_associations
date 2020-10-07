c2a12284cf288fdc04788b56f07386e621a487ce
ClionaOD <odoherc1@tcd.ie>
2020-10-07 21:09:45 +0100

    open lch

--- get_lch.sh
+++ get_lch.sh
@@ -6,6 +6,6 @@ python3 /home/clionaodoherty/cmc_associations/lch_comparison.py \
     --image_path /data/imagenet_cmc/to_test \
     --save_path /home/clionaodoherty/cmc_associations/results/256_class_results.pickle \
     --rdm_path /home/clionaodoherty/cmc_associations/rdms \
-    --save_lch True \
+    --open_lch True \
     --lch_path /home/clionaodoherty/cmc_associations \
     --correlation spearman
