12d3254ad0b7f7ff62dc7348437923b30db6987f
ClionaOD <odoherc1@tcd.ie>
2020-10-07 14:23:34 +0100

    copy 200 classes

--- move_images.py
+++ move_images.py
@@ -8,7 +8,7 @@ wnids = os.listdir(base_path)
 wnids = [wnid for wnid in wnids if not 'json' in wnid and not 'to_test' in wnid]
 wnids = [wnid for wnid in wnids if len(os.listdir(os.path.join(base_path,wnid))) == 150]
 mv_wnids = []
-if len(wnids) > 256:
+if len(wnids) > 200:
     while len(mv_wnids) < 256:
         choice = random.choice(wnids)
         if not choice in mv_wnids:
@@ -17,4 +17,4 @@ if len(wnids) > 256:
 for wnid in mv_wnids:
     source = os.path.join(base_path,wnid)
     dest = os.path.join(base_path,'to_test')
-    shutil.move(source,dest)
+    shutil.copy(source,dest)
