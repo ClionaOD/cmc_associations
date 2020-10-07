39a5124b4362ac320658aacd3be53c9bba38924a
ClionaOD <odoherc1@tcd.ie>
2020-10-07 09:38:34 +0100

    choose lower n images

--- get_imagenet.py
+++ get_imagenet.py
@@ -51,7 +51,7 @@ if manual == True:
     wnids = os.listdir(category_path)
     wnids = [i for i in wnids if not 'json' in i and not 'to_test' in i]
     lens = {wnid: len(os.listdir(f'{category_path}/{wnid}')) for wnid in wnids}
-    lens = {k:v for k,v in lens.items() if v >= 140 and v < 150}
+    lens = {k:v for k,v in lens.items() if  v < 20}
     test_categs = list(lens.keys())
 
 if not manual:
