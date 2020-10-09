227b87d5bd6e053f83970d77fbebd79b2df183c1
ClionaOD <odoherc1@tcd.ie>
2020-10-09 11:37:46 +0100

    update for 5min

--- get_acts.sh
+++ get_acts.sh
@@ -3,7 +3,7 @@
 
 #WORKSTATION PATHS
 python3 /home/clionaodoherty/cmc_associations/get_activations.py \
-   --model_path /home/clionaodoherty/cmc_associations/weights \
-   --save_path  /home/clionaodoherty/cmc_associations/activations \
+   --model_path /home/clionaodoherty/cmc_associations/weights/5min \
+   --save_path  /home/clionaodoherty/cmc_associations/activations/5min \
    --image_path /data/imagenet_cmc/to_test \
    --transform distort 
--- get_rdms.sh
+++ get_rdms.sh
@@ -3,6 +3,6 @@
 #WORKSTATION PATHS FOR RDMS
 python3 /home/clionaodoherty/cmc_associations/get_rdms.py \
     --image_path /data/imagenet_cmc/to_test \
-    --activation_path /home/clionaodoherty/cmc_associations/activations \
+    --activation_path /home/clionaodoherty/cmc_associations/activations/5min \
     --rdm_path /home/clionaodoherty/cmc_associations/rdms \
     --save_rdm True
