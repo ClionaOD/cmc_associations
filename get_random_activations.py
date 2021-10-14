b333512748797fffa587adfcab1d476660b3493d
ClionaOD <odoherc1@tcd.ie>
2021-10-14 17:55:24 +0100

    cleaner iterating over transforms

--- get_random_activations.py
+++ get_random_activations.py
@@ -6,8 +6,8 @@ import torchvision.transforms as transforms
 from dataset import RGB2Lab
 import os
 import torch
-from torchvision.models import alexnet
 from models.alexnet import TemporalAlexNetCMC
+from torchvision.models import alexnet
 import pickle
 
 def main(args, model_weights=''):
@@ -18,35 +18,30 @@ def main(args, model_weights=''):
     if not _file == 'random':
         raise Error('The weights provided are not random please check paths')
     
+    modelpth = os.path.join(args.model_path, model_weights)
+
+    # Only need this syntax because will load random only
+    checkpoint = torch.load(modelpth)
+
+    model = TemporalAlexNetCMC()
+    model.load_state_dict(checkpoint)
+    model.cuda()
+
     if not args.supervised:
-        print('not supervised')
-        modelpth = os.path.join(args.model_path, model_weights)
-
-        # Only need this syntax because will load random only
-        checkpoint = torch.load(modelpth)
-
-        model = TemporalAlexNetCMC()
-        model.load_state_dict(checkpoint)
-        model.cuda()
-
         activations = get_activations(args.image_path, model, args)
     else:
-        print('supervised')
-        model = alexnet(pretrained=True)
-        model.cuda()
-
         activations = get_activations(args.image_path, model, args, mean=[0.485,0.456,0.406], std=[0.229, 0.224, 0.225])
 
     print('done ... saving')
 
     if not args.supervised:
-        _save = f'{args.save_path}/{_file}_{args.transform}_activations.pickle'
+        _save = f'{args.save_path}/{_file}-{args.transform}_activations.pickle'
         if args.blur:
-            _save = f'{args.save_path}/{_file}_blur_sigma{args.sigma}_kernel{args.kernel_size}_activations.pickle'
+            _save = f'{args.save_path}/{_file}-{args.transform}_blur_sigma{args.sigma}_kernel{args.kernel_size}_activations.pickle'
     else:
-        _save = f'{args.save_path}/{_file}_supervised_activations.pickle'
+        _save = f'{args.save_path}/{_file}-supervised_activations.pickle'
         if args.blur:
-            _save = f'{args.save_path}/supervised_blur_sigma{args.sigma}_kernel{args.kernel_size}_activations.pickle'
+            _save = f'{args.save_path}/{_file}-supervised_blur_sigma{args.sigma}_kernel{args.kernel_size}_activations.pickle'
     
     with open(_save, 'wb') as handle:
         pickle.dump(activations, handle)
@@ -54,16 +49,12 @@ def main(args, model_weights=''):
 if __name__ == '__main__':
     args = parse_option()
 
-    args.model_path = '/data/movie-associations/weights_for_eval/main'
-    args.save_path = '/data/movie-associations/activations...'
-    args.image_path = '/data/movie-associations/imagenet_cmc_256/to_test'
-
     colors = ['distort', 'Lab', 'distort'] # [-1] is distort because default but this won't be used
-    models = ['random_alexnet.pth', 'random_alexnet.pth', '']
+    model = 'random_alexnet.pth'
     sup = [False, False, True]
     
     for i in range(3):
         args.transform = colors[i]
         args.supervised = sup[i]
         
-        main(args, models[0])
+        main(args, model)
